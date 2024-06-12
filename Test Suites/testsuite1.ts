bb763feb3bc1dad9106ac5fbe5165a213556c5be
Nadia <Nadia@L19PC0AGC2P.dlinkrouter.local.>
2024-06-12 14:52:10 +0800

    Added test suite

---
+++ Test Suites/testsuite1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>002adb48-9129-436f-b230-c96df0fdfe8e</testSuiteGuid>
+   <testCaseLink>
+      <guid>ad2ba2ae-bc9e-43eb-9caa-55f246f40ebd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Automate_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
